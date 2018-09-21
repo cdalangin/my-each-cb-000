1818acb93ff237ea8266c4bdcc93166982c442db
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-21 02:03:59 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each(array) # put argument(s) here
   # code here
   array = []
   contents = array.length
-  while contents > 0 { |words|
+  while contents > 0 |words|
     array.length-= 1
     words
-  }
+  end
 end
